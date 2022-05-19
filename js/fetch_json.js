10de4023948982eb781342acdd96700da8ef99c6
Mike Ambani <63499020+m0rph3u5-eng@users.noreply.github.com>
2022-05-19 01:19:50 -0700

    data query from JSON files

--- js/fetch_json.js
+++ js/fetch_json.js
@@ -1,4 +1,5 @@
-console.log("fetch json");
+let user;
+let order;
 
 fetch("w_list.json")
   .then(function (response) {
@@ -13,12 +14,6 @@ fetch("w_list.json")
 
 function appendData(data) {
   for (i = 0; i < data.length; i++) {
-    //console.log(data[i].type);
-    //console.log(data[i].title);
-    // console.log(data[i].user);
-    // console.log(data[i].description);
-    // console.log(data[i].cost);
-
     const myHeader = document.createElement("h6");
     const myUser = document.createElement("p");
     const myDesc = document.createElement("p");
@@ -35,7 +30,6 @@ function appendData(data) {
       "list-group-item list-group-item-action d-flex gap-3 py-3";
     myCost.className = "opacity-50 text-nowrap";
     myLink.href = "#";
-   // myLink.addEventListener("click", call);
 
     myLink.ariaCurrent = true;
     myHeader.innerHTML = data[i].title;
@@ -43,7 +37,96 @@ function appendData(data) {
     myDesc.innerHTML = data[i].description;
     myCost.innerHTML = data[i].cost;
 
-   
+    myLink.onclick = function setTitle() {
+      order = [myHeader.innerHTML];
+
+      var mail = [];
+      user = [myUser.innerHTML];
+      mail.push("mailto:nancy@example.com?subject=shoppinglist&body=bargain");
+
+      fetch("w_list_items.json")
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          appendData(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      function appendData(data) {
+        if (myHeader.innerHTML == data[0].list) {
+          //console.log("Match");
+        } else {
+          //console.log("No Match");
+        }
+        document.getElementById("heading").innerHTML = myHeader.innerHTML;
+        document.getElementById("handle").innerHTML = myUser.innerHTML;
+        document.getElementById("price0").innerHTML = myCost.innerHTML;
+        //div newnew
+        var newdiv2 = document.getElementById("newnew");
+        newdiv2.innerHTML = "";
+        for (i = 0; i < data.length; i++) {
+          //= data.find(data => data.list === myHeader.innerHTML);
+
+          if (data[i].list === myHeader.innerHTML) {
+            //console.log(data[i].item);
+            const myNewItem = document.createElement("p");
+            myNewItem.className = "mb-0 opacity-75";
+            myNewItem.innerHTML = data[i].item;
+            document.getElementById("newnew").appendChild(myNewItem);
+          } else {
+          }
+        }
+      }
+      function comment21() {
+        fetch("w_comments.json")
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (datac) {
+            appendData(datac);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+
+        function appendData(datac) {
+          //div newnew
+          //var newdiv3 = document.getElementById('newc');
+          var newdivcom = document.getElementById("newc");
+          newdivcom.innerHTML="";
+          for (i = 0; i < datac.length; i++) {
+            if (datac[i].list == myHeader.innerHTML) {
+            //   console.log(datac[i].comment);
+            //   console.log(datac[i].time);
+              const myNewItem = document.createElement("p");
+              const myNewA = document.createElement("a");
+              const myNewComDiv = document.createElement("div");
+              const myNewNestComDiv = document.createElement("div");
+              const myNewSmall = document.createElement("small");
+              myNewA.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+              myNewComDiv.className = "d-flex gap-2 w-100 justify-content-between";
+              myNewSmall.className ="opacity-50 text-nowrap";
+              myNewItem.className = "mb-0 opacity-75";
+              myNewItem.innerHTML = "<b>"+ datac[i].name+ "</b>" + " " +datac[i].comment;
+              myNewSmall.innerHTML =datac[i].time;
+             
+              myNewNestComDiv.appendChild(myNewItem);
+              
+              myNewComDiv.appendChild(myNewNestComDiv);
+              myNewComDiv.appendChild(myNewSmall);
+              myNewA.appendChild(myNewComDiv);
+
+              document.getElementById("newc").appendChild(myNewA);
+            } else {
+            }
+          }
+        }
+      }
+      comment21();
+    };
 
     myHeader.appendChild(myUser);
     myDivPrime.appendChild(myHeader);
@@ -57,39 +140,40 @@ function appendData(data) {
   }
 }
 
-
 function comment21() {
-    fetch("w_comments.json")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (datac) {
-        appendData(datac);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+  fetch("w_comments.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (datac) {
+      appendData(datac);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 
-    function appendData(datac) {
+  function appendData(datac) {
+    //div newnew
+    //var newdiv3 = document.getElementById('newc');
 
-      //div newnew
-      //var newdiv3 = document.getElementById('newc');
-
-
-      for (i = 0; i < datac.length; i++) {
-
-        if (datac[i].user == "@Nancy") {
-          console.log(datac[i].comment);
-          console.log(datac[i].time);
-          // const myNewItem = document.createElement("p");
-          // myNewItem.className = "mb-0 opacity-75"
-          // myNewItem.innerHTML = data[i].item;
-          // document.getElementById("newc").appendChild(myNewItem);
-
-        } else {
-
-        }
-
+    for (i = 0; i < datac.length; i++) {
+      if (datac[i].user == "@Nancy") {
+        console.log(datac[i].comment);
+        console.log(datac[i].time);
+        // const myNewItem = document.createElement("p");
+        // myNewItem.className = "mb-0 opacity-75"
+        // myNewItem.innerHTML = data[i].item;
+        // document.getElementById("newc").appendChild(myNewItem);
+      } else {
       }
     }
-  } 
+  }
+}
+function orderf() {
+  if (
+    confirm("Do you want to place an order to " + user + " for " + order + "?")
+  ) {
+    window.open();
+  } else {
+  }
+}
--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@
   var price = document.getElementById("price0");
   var comm = document.getElementById("oldc");
   if (x.style.display == "none") {
-
     x.style.display = "block";
     xl.style.display = "block";
     ml.style.display = "none";
@@ -16,9 +15,8 @@
     comm.style.display = "none";
     xa.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
     xla.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
-    document.getElementById('plus').innerHTML = "<h4>-</h4>";
+    document.getElementById("plus").innerHTML = "<h4>-</h4>";
   } else {
-
     x.style.display = "none";
     xl.style.display = "none";
     xa.className = "list-group-item list-group-item-action gap-3 py-3";
@@ -26,82 +24,136 @@
     ml.style.display = "block";
     price.style.display = "block";
     comm.style.display = "block";
-    document.getElementById('plus').innerHTML = "<h4>+</h4>";
+    document.getElementById("plus").innerHTML = "<h4>+</h4>";
   }
 }
 
 var list2 = [];
-                function saveLi() {
+function saveLi() {
+  var title1 = document.getElementById("title").value;
+  var desc1 = document.getElementById("t_desc").value;
+  var newdiv1 = document.getElementById("new_list1");
 
-                  var title1 = document.getElementById('title').value;
-                  var desc1 = document.getElementById('t_desc').value;
-                  var newdiv1 = document.getElementById('new_list1');
+  console.log(title1);
+  console.log(desc1);
 
-                  console.log(title1);
-                  console.log(desc1);
+  //alert("Your list has been saved!");
 
-                  //alert("Your list has been saved!");
+  //document.getElementById('new_list1').innerHTML = title1;
+  const myHeader = document.createElement("h6");
+  const myUser = document.createElement("p");
+  const myDesc = document.createElement("p");
+  const myDiv = document.createElement("div");
+  const myDivPrime = document.createElement("div");
+  const myLink = document.createElement("a");
+  const myCost = document.createElement("small");
 
-                  //document.getElementById('new_list1').innerHTML = title1;
-                  const myHeader = document.createElement("h6");
-                  const myUser = document.createElement("p");
-                  const myDesc = document.createElement("p");
-                  const myDiv = document.createElement("div");
-                  const myDivPrime = document.createElement("div");
-                  const myLink = document.createElement("a");
-                  const myCost = document.createElement("small");
+  myHeader.className = "mb-0";
+  myUser.className = "mb-0 opacity-75";
+  myDesc.className = "mb-0 opacity-75";
+  myDiv.className = "d-flex gap-2 w-100 justify-content-between";
+  myLink.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+  myCost.className = "opacity-50 text-nowrap";
+  myLink.href = "#";
+  myLink.addEventListener("click", call);
 
-                  myHeader.className = "mb-0";
-                  myUser.className = "mb-0 opacity-75";
-                  myDesc.className = "mb-0 opacity-75";
-                  myDiv.className = "d-flex gap-2 w-100 justify-content-between";
-                  myLink.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
-                  myCost.className = "opacity-50 text-nowrap";
-                  myLink.href = "#";
-                  myLink.addEventListener('click', call);
+  myLink.ariaCurrent = true;
 
-                  myLink.ariaCurrent = true;
+  myHeader.innerHTML = title1;
+  myUser.innerHTML = "@Simon";
+  myDesc.innerHTML = desc1;
+  myCost.innerHTML = 1250;
 
+  myHeader.appendChild(myUser);
+  myDivPrime.appendChild(myHeader);
+  myDivPrime.appendChild(myUser);
+  myDivPrime.appendChild(myDesc);
+  myDiv.appendChild(myDivPrime);
+  myDiv.appendChild(myCost);
+  myLink.appendChild(myDiv);
 
-                  myHeader.innerHTML = title1;
-                  myUser.innerHTML = "@Simon";
-                  myDesc.innerHTML = desc1;
-                  myCost.innerHTML = 1250;
+  document.getElementById("new_list1").appendChild(myLink);
+  new_entry();
 
+  console.log(list2);
+  document.getElementById("heading").innerHTML = title1;
+  document.getElementById("handle").innerHTML = "@Simon";
+  //div newnew
+  var newdiv2 = document.getElementById("newnew");
 
+  for (i = 0; i < list2.length; i++) {
+    const myNewItem = document.createElement("p");
+    myNewItem.className = "mb-0 opacity-75";
+    myNewItem.innerHTML = list2[i];
+    document.getElementById("newnew").appendChild(myNewItem);
+  }
+  document.getElementById("item0").innerHTML = "";
+  document.getElementById("item1").innerHTML = "";
+  document.getElementById("item2").innerHTML = "";
+  document.getElementById("item3").innerHTML = "";
+  document.getElementById("item4").innerHTML = "";
+  document.getElementById("item5").innerHTML = "";
+  document.getElementById("item6").innerHTML = "";
+  document.getElementById("item7").innerHTML = "";
+  document.getElementById("item8").innerHTML = "";
+  document.getElementById("item9").innerHTML = "";
+}
 
-                  myHeader.appendChild(myUser);
-                  myDivPrime.appendChild(myHeader);
-                  myDivPrime.appendChild(myUser);
-                  myDivPrime.appendChild(myDesc);
-                  myDiv.appendChild(myDivPrime);
-                  myDiv.appendChild(myCost)
-                  myLink.appendChild(myDiv);
+function addLi() {
+  var title1 = [];
+  var txtVal = document.getElementById("txtVal").value,
+    listNode = document.getElementById("list1"),
+    liNode = document.createElement("li"),
+    txtNode = document.createTextNode(txtVal),
+    title = document.getElementById("title").value;
+  description = document.getElementById("t_desc").value;
+  liNode.appendChild(txtNode);
+  listNode.appendChild(liNode);
+  list2.push(txtVal);
+  //console.log(list2);
+  document.getElementById("newHeader").innerHTML = title;
 
-                  document.getElementById("new_list1").appendChild(myLink);
-                  new_entry();
+  title_main = title;
+  //list_item.push(txtVal);
+}
 
-                  console.log(list2);
-                  document.getElementById('heading').innerHTML = title1;
-                  document.getElementById('handle').innerHTML = "@Simon";
-                  //div newnew
-                  var newdiv2 = document.getElementById('newnew');
+function clearLi() {
+  t_list = [];
+  title_main = [];
+  description = [];
+  list_item = [];
+  recipe = [];
+  document.getElementById("list1").innerHTML = "";
+  console.log("List cleared");
+}
 
-                  for (i = 0; i < list2.length; i++) {
-                    const myNewItem = document.createElement("p");
-                    myNewItem.className = "mb-0 opacity-75"
-                    myNewItem.innerHTML = list2[i];
-                    document.getElementById("newnew").appendChild(myNewItem);
+function addcom() {
+  comments1 = document.getElementById('comments2').value;
+  console.log(comments1);
 
-                  }
-                  document.getElementById('item0').innerHTML = "";
-                  document.getElementById('item1').innerHTML = "";
-                  document.getElementById('item2').innerHTML = "";
-                  document.getElementById('item3').innerHTML = "";
-                  document.getElementById('item4').innerHTML = "";
-                  document.getElementById('item5').innerHTML = "";
-                  document.getElementById('item6').innerHTML = "";
-                  document.getElementById('item7').innerHTML = "";
-                  document.getElementById('item8').innerHTML = "";
-                  document.getElementById('item9').innerHTML = "";
-                }
+  const usecom = document.createElement("p");
+  const usetim = document.createElement("p");
+  const divcom = document.createElement("div");
+  const acom = document.createElement("a");
+  usecom.className = "mb-0 opacity-75";
+  usetim.className = "opacity-50 text-nowrap";
+  divcom.className = "d-flex gap-2 w-100 justify-content-between";
+  acom.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+  acom.ariaCurrent = true;
+  divcom.id = "newc1";
+  usecom.innerHTML = "<b>@Simon </b>: " + comments1;
+  usetim.innerHTML = "Now";
+
+  document.getElementById("newc").appendChild(acom);
+  acom.appendChild(divcom);
+  divcom.appendChild(usecom);
+  divcom.appendChild(usetim);
+
+  localStorage.setItem("list", JSON.stringify(order[0]));
+  localStorage.setItem("comments", JSON.stringify(comments1));
+  //var storedComments = JSON.parse(localStorage.getItem("comments"+"list"));
+  console.log("BREAK");
+  localStorage.comments1 = JSON.stringify(comments1);
+  var storedComments = JSON.parse(localStorage.comments1);
+  console.log(storedComments);
+}
