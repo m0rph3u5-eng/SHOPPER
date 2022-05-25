e44b0f3f6da5538b9337989ed0a7847a551fa252
Mike Ambani <63499020+m0rph3u5-eng@users.noreply.github.com>
2022-05-25 01:30:48 -0700

    fixed default display

--- js/fetch_json.js
+++ js/fetch_json.js
@@ -96,25 +96,28 @@ function appendData(data) {
           //div newnew
           //var newdiv3 = document.getElementById('newc');
           var newdivcom = document.getElementById("newc");
-          newdivcom.innerHTML="";
+          newdivcom.innerHTML = "";
           for (i = 0; i < datac.length; i++) {
             if (datac[i].list == myHeader.innerHTML) {
-            //   console.log(datac[i].comment);
-            //   console.log(datac[i].time);
+              //   console.log(datac[i].comment);
+              //   console.log(datac[i].time);
               const myNewItem = document.createElement("p");
               const myNewA = document.createElement("a");
               const myNewComDiv = document.createElement("div");
               const myNewNestComDiv = document.createElement("div");
               const myNewSmall = document.createElement("small");
-              myNewA.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
-              myNewComDiv.className = "d-flex gap-2 w-100 justify-content-between";
-              myNewSmall.className ="opacity-50 text-nowrap";
+              myNewA.className =
+                "list-group-item list-group-item-action d-flex gap-3 py-3";
+              myNewComDiv.className =
+                "d-flex gap-2 w-100 justify-content-between";
+              myNewSmall.className = "opacity-50 text-nowrap";
               myNewItem.className = "mb-0 opacity-75";
-              myNewItem.innerHTML = "<b>"+ datac[i].name+ "</b>" + " " +datac[i].comment;
-              myNewSmall.innerHTML =datac[i].time;
-             
+              myNewItem.innerHTML =
+                "<b>" + datac[i].name + "</b>" + " " + datac[i].comment;
+              myNewSmall.innerHTML = datac[i].time;
+
               myNewNestComDiv.appendChild(myNewItem);
-              
+
               myNewComDiv.appendChild(myNewNestComDiv);
               myNewComDiv.appendChild(myNewSmall);
               myNewA.appendChild(myNewComDiv);
@@ -128,6 +131,96 @@ function appendData(data) {
       comment21();
     };
 
+    function setDefaultTitle() {
+      order = [data[0].title];
+
+      var mail = [];
+      user = [data[0].user];
+      mail.push("mailto:nancy@example.com?subject=shoppinglist&body=bargain");
+
+      fetch("w_list_items.json")
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          appendData(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      function appendData(data) {
+  
+        document.getElementById("heading").innerHTML = data[0].list;
+        document.getElementById("handle").innerHTML = data[0].user;
+        document.getElementById("price0").innerHTML = data[0].cost;
+        //div newnew
+        var newdiv2 = document.getElementById("newnew");
+        newdiv2.innerHTML = "";
+        for (i = 0; i < data.length; i++) {
+          //= data.find(data => data.list === myHeader.innerHTML);
+
+          if (data[i].list === myHeader.innerHTML) {
+            //console.log(data[i].item);
+            const myNewItem = document.createElement("p");
+            myNewItem.className = "mb-0 opacity-75";
+            myNewItem.innerHTML = data[i].item;
+            document.getElementById("newnew").appendChild(myNewItem);
+          } else {
+          }
+        }
+      }
+      function comment21() {
+        fetch("w_comments.json")
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (datac) {
+            appendData(datac);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+
+        function appendData(datac) {
+          //div newnew
+          //var newdiv3 = document.getElementById('newc');
+          var newdivcom = document.getElementById("newc");
+          newdivcom.innerHTML = "";
+          for (i = 0; i < datac.length; i++) {
+            if (datac[i].list == myHeader.innerHTML) {
+              //   console.log(datac[i].comment);
+              //   console.log(datac[i].time);
+              const myNewItem = document.createElement("p");
+              const myNewA = document.createElement("a");
+              const myNewComDiv = document.createElement("div");
+              const myNewNestComDiv = document.createElement("div");
+              const myNewSmall = document.createElement("small");
+              myNewA.className =
+                "list-group-item list-group-item-action d-flex gap-3 py-3";
+              myNewComDiv.className =
+                "d-flex gap-2 w-100 justify-content-between";
+              myNewSmall.className = "opacity-50 text-nowrap";
+              myNewItem.className = "mb-0 opacity-75";
+              myNewItem.innerHTML =
+                "<b>" + datac[i].name + "</b>" + " " + datac[i].comment;
+              myNewSmall.innerHTML = datac[i].time;
+
+              myNewNestComDiv.appendChild(myNewItem);
+
+              myNewComDiv.appendChild(myNewNestComDiv);
+              myNewComDiv.appendChild(myNewSmall);
+              myNewA.appendChild(myNewComDiv);
+
+              document.getElementById("newc").appendChild(myNewA);
+            } else {
+            }
+          }
+        }
+      }
+      comment21();
+    };
+    setDefaultTitle();
     myHeader.appendChild(myUser);
     myDivPrime.appendChild(myHeader);
     myDivPrime.appendChild(myUser);
@@ -140,35 +233,7 @@ function appendData(data) {
   }
 }
 
-function comment21() {
-  fetch("w_comments.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (datac) {
-      appendData(datac);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
 
-  function appendData(datac) {
-    //div newnew
-    //var newdiv3 = document.getElementById('newc');
-
-    for (i = 0; i < datac.length; i++) {
-      if (datac[i].user == "@Nancy") {
-        console.log(datac[i].comment);
-        console.log(datac[i].time);
-        // const myNewItem = document.createElement("p");
-        // myNewItem.className = "mb-0 opacity-75"
-        // myNewItem.innerHTML = data[i].item;
-        // document.getElementById("newc").appendChild(myNewItem);
-      } else {
-      }
-    }
-  }
-}
 function orderf() {
   if (
     confirm("Do you want to place an order to " + user + " for " + order + "?")
