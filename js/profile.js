7e7d76172653763feefcb37a72162d87aafb2fa2
Mike Ambani <63499020+m0rph3u5-eng@users.noreply.github.com>
2022-07-22 15:17:23 -0700

    Add files via upload

---
+++ js/profile.js
@@ -0,0 +1,79 @@
+function age0to2() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age0to2check");
+    // Get the output text
+    var text = document.getElementById("age0to2list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function age3to12() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age3to12check");
+    // Get the output text
+    var text = document.getElementById("age3to12list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function age13to18() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age13to18check");
+    // Get the output text
+    var text = document.getElementById("age13to18list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function age19to35() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age19to35check");
+    // Get the output text
+    var text = document.getElementById("age19to35list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function age36to64() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age36to64check");
+    // Get the output text
+    var text = document.getElementById("age36to64list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function age65() {
+    // Get the checkbox
+    var checkBox = document.getElementById("age65check");
+    // Get the output text
+    var text = document.getElementById("age65list");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  
