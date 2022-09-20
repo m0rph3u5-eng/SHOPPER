81091fd31aa6e8b39b829343fbb658c2689a20f1
Mike Ambani <63499020+m0rph3u5-eng@users.noreply.github.com>
2022-09-20 14:45:31 +0300

    picture capabilities

--- js/fetch_json.js
+++ js/fetch_json.js
@@ -2,6 +2,9 @@ let user;
 let order;
 let mail;
 let recipeCheck = document.title;
+let allUsers = [];
+let selectedUser =[];
+let allCommenters = [];
 //console.log(recipeCheck);
 
 function setDefaultTitle() {
@@ -23,37 +26,44 @@ function setDefaultTitle() {
     // let img1 = document.createElement("img");
     // img1.src="images/"+0+".jpg";
     // document.getElementById("img1").appendChild(img1);
+    let myAvi1 = document.createElement("img");
+    myAvi1.className = "img-fluid";
+    myAvi1.style = "width:50px; height:50px; float:left; margin-right:10px; border: 0px solid red; border-radius: 50%;";
+    myAvi1.src = "images/avatars/0.jpg";
+
     document.getElementById("heading").innerHTML = data[0].list;
+    document.getElementById("heading").appendChild(myAvi1);
     document.getElementById("handle").innerHTML = data[0].user;
     document.getElementById("price0").innerHTML = data[0].cost;
 
+    selectedUser = data[0].user;
     // newdiv2.className = "d-flex gap-2 w-100 justify-content-between w-100";
     const myOl = document.createElement("ol");
-    myOl.className = "list-group list-group-numbered w-100 d-flex p-0 scrollbar scrollbar-black bordered-black square thin border border-light rounded-lg overflow-auto";
+    myOl.className =
+      "list-group list-group-numbered w-100 d-flex p-0 scrollbar scrollbar-black bordered-black square thin border border-light rounded-lg overflow-auto";
 
-  
     for (i = 0; i < data.length; i++) {
       if (data[i].list == data[0].list) {
-       const myNewItem = document.createElement("li");
-       const myItImg = document.createElement("img");
-        myNewItem.className = "list-group-item list-group-item-action mb-0 opacity-75";
-        myItImg.src = "images/items/"+[i]+".jpg";
+        const myNewItem = document.createElement("li");
+        const myItImg = document.createElement("img");
+        myNewItem.className =
+          "list-group-item list-group-item-action mb-0 opacity-75";
+        myItImg.src = "images/items/" + [i] + ".jpg";
         myItImg.className = "img-fluid";
-        myItImg.style = "width:40px; float:left;padding-right:5px;"
+        myItImg.style = "width:40px; float:left;padding-right:5px;";
         myNewItem.innerHTML = data[i].item;
-        
+
         //change bachground colour when item is clicked
-        myNewItem.onclick = function colorChange() {         
-          myNewItem.className="list-group-item active border border-secondary" 
-      }
-      myNewItem.appendChild(myItImg);  
-      myOl.appendChild(myNewItem);
+        myNewItem.onclick = function colorChange() {
+          myNewItem.className =
+            "list-group-item active border border-secondary";
+        };
+        myNewItem.appendChild(myItImg);
+        myOl.appendChild(myNewItem);
       }
     }
 
     document.getElementById("newnew").appendChild(myOl);
-
-    
   }
   function comment22() {
     fetch("w_comments.json")
@@ -76,11 +86,19 @@ function setDefaultTitle() {
         if (datac[i].list == datac[0].list) {
           //   console.log(datac[i].comment);
           //   console.log(datac[i].time);
+
           const myNewItem = document.createElement("p");
           const myNewA = document.createElement("a");
           const myNewComDiv = document.createElement("div");
           const myNewNestComDiv = document.createElement("div");
           const myNewSmall = document.createElement("small");
+
+          let myAvi1 = document.createElement("img");
+          myAvi1.className = "img-fluid";
+          myAvi1.style =
+            "width:50px; height:50px; float:left; margin-right:10px; border: 0px solid red; border-radius: 50%;";
+          myAvi1.src = "images/avatars/1" + [i] + ".jpg";
+
           myNewA.className =
             "list-group-item list-group-item-action d-flex gap-3 py-3";
           myNewComDiv.className = "d-flex gap-2 w-100 justify-content-between";
@@ -90,8 +108,9 @@ function setDefaultTitle() {
             "<b>" + datac[i].name + "</b>" + "<br>" + datac[i].comment;
           myNewSmall.innerHTML = datac[i].time;
 
+          allCommenters.push(datac[i].name);
           myNewNestComDiv.appendChild(myNewItem);
-
+          myNewComDiv.appendChild(myAvi1);
           myNewComDiv.appendChild(myNewNestComDiv);
           myNewComDiv.appendChild(myNewSmall);
           myNewA.appendChild(myNewComDiv);
@@ -131,19 +150,19 @@ function setDefaultTitle() {
           //const myNewComDiv = document.createElement("div");
           const myNewNestComDiv1 = document.createElement("div");
           //const myNewSmall = document.createElement("small");
-         // myNewA.className =
-            "list-group-item list-group-item-action d-flex gap-3 py-3";
+          // myNewA.className =
+          ("list-group-item list-group-item-action d-flex gap-3 py-3");
           //myNewComDiv.className =
-            "d-flex gap-2 w-100 justify-content-between";
-         // myNewSmall.className = "opacity-50 text-nowrap";
+          ("d-flex gap-2 w-100 justify-content-between");
+          // myNewSmall.className = "opacity-50 text-nowrap";
           myNewItem1.className = "mb-0 opacity-75";
-          myNewItem1.innerHTML ="<b>Step "+ rnum++ +"</b>"+"<br>"+datac[i].recipe;
-          
+          myNewItem1.innerHTML =
+            "<b>Step " + rnum++ + "</b>" + "<br>" + datac[i].recipe;
 
           myNewNestComDiv1.appendChild(myNewItem1);
 
           //myNewComDiv.appendChild(myNewNestComDiv);
-         /// myNewComDiv.appendChild(myNewSmall);
+          /// myNewComDiv.appendChild(myNewSmall);
           //myNewA.appendChild(myNewComDiv);
 
           document.getElementById("newr").appendChild(myNewNestComDiv1);
@@ -152,10 +171,9 @@ function setDefaultTitle() {
       }
     }
   }
-  if (recipeCheck=="Recipes | SHOPPER") {
+  if (recipeCheck == "Recipes | SHOPPER") {
     recipe1();
   }
-  
 }
 setDefaultTitle();
 //fetch for first column data from json file
@@ -173,7 +191,7 @@ fetch("w_list.json")
 function appendData(data) {
   for (i = 0; i < data.length; i++) {
     //create elements
-  
+
     const myHeader = document.createElement("h6");
     const myUser = document.createElement("p");
     const myDesc = document.createElement("p");
@@ -184,10 +202,12 @@ function appendData(data) {
     const myImg = document.createElement("img");
     const myAvi = document.createElement("img");
 
+
     //apply styling to the elements
     myImg.className = "img-fluid";
     myAvi.className = "img-fluid";
-    myAvi.style= "width:50px; float:left; padding-right:10px; border: 0px solid red; border-radius: 100px;"
+    myAvi.style =
+      "width:40px; height:40px; float:left; margin-right:10px; border-radius: 50%;";
     myHeader.className = "mb-0";
     myUser.className = "mb-0 opacity-75";
     myDesc.className = "mb-0 opacity-75";
@@ -198,9 +218,10 @@ function appendData(data) {
     myLink.ariaCurrent = true;
 
     //apply values to the created elements
-    
-    myAvi.src = "images/avatars/"+[i]+".jpg";
-    myImg.src = "images/"+[i]+".jpg";
+
+    allUsers.push(data[i].user);
+    myAvi.src = "images/avatars/" + [i] + ".jpg";
+    myImg.src = "images/" + [i] + ".jpg";
     myLink.href = "#";
     myHeader.innerHTML = data[i].title;
     myUser.innerHTML = data[i].user;
@@ -208,32 +229,27 @@ function appendData(data) {
     myCost.innerHTML = data[i].cost;
 
     myHeader.appendChild(myUser);
-    myDivPrime.appendChild(myAvi);
+    //myDivPrime.appendChild(myAvi);
     myDivPrime.appendChild(myHeader);
+    myDivPrime.appendChild(myAvi);
     myDivPrime.appendChild(myUser);
     myDivPrime.appendChild(myDesc);
     myDiv.appendChild(myDivPrime);
     myDiv.appendChild(myCost);
-  // myLink.appendChild(myImg);
-   //  myDivPrime.appendChild(myImg);
+    //myDiv.appendChild(myAvi);
+    // myLink.appendChild(myImg);
+    //  myDivPrime.appendChild(myImg);
     myLink.appendChild(myDiv);
- 
 
-  document.getElementById("des_col").appendChild(myImg);
+    document.getElementById("des_col").appendChild(myImg);
     document.getElementById("des_col").appendChild(myLink);
-    
 
-    
-    
     //show list items when list type is selected
     myLink.onclick = function setTitle() {
-     
       //save  values in variables
       order = [myHeader.innerHTML];
 
       user = [myUser.innerHTML];
-     
-    
 
       //fetch for second column data from json file
       fetch("w_list_items.json")
@@ -252,26 +268,31 @@ function appendData(data) {
         document.getElementById("handle").innerHTML = myUser.innerHTML;
         document.getElementById("price0").innerHTML = myCost.innerHTML;
 
-         //change image source
-     var imgSelect = data[i].id;
-     let img2 = document.getElementById("img1");
-    
-     img2.src="images/"+imgSelect+".jpg";
-     const myAvi = document.createElement("img");
-     myAvi.className = "img-fluid";
-     myAvi.style= "width:50px; float:left; padding-right:10px; border: 0px solid red; border-radius: 100px;"  
-     myAvi.src = "images/avatars/"+[i]+".jpg";
-     // mail = [data[i].whatsapp];
+        selectedUser = myUser.innerHTML;
+        console.log(selectedUser);
+
+        //change image source
+        //var imgSelect = data[i].id;
+        let img2 = document.getElementById("img1");
+
+        img2.src = "images/" + allUsers.indexOf(selectedUser)+ ".jpg";
+        const myAvi = document.createElement("img");
+        myAvi.className = "img-fluid";
+        myAvi.style = "width:50px; float:left; padding-right:10px; border: 0px solid red; border-radius: 100px;";
+        myAvi.src = "images/avatars/" + allUsers.indexOf(selectedUser)+ ".jpg";
+        // mail = [data[i].whatsapp];
         // console.log(data[i].whatsapp);
         var newdiv2 = document.getElementById("newnew");
         newdiv2.innerHTML = "";
-        newdiv2.className = "d-flex gap-2 w-100 justify-content-between w-100 overflow-auto flex-column mb-auto";
+        newdiv2.className =
+          "d-flex gap-2 w-100 justify-content-between w-100 overflow-auto flex-column mb-auto";
         //newdiv2.appendChild(myAvi);
         document.getElementById("heading").appendChild(myAvi);
         const myOl = document.createElement("ol");
-        myOl.className = "list-group list-group-numbered w-100 d-flex p-0 scrollbar scrollbar-black bordered-black square thin border border-light rounded-lg overflow-auto flex-column mb-auto";
-// myOl.style.width ="inherit"
-// myOl.style.height ="inherit"
+        myOl.className =
+          "list-group list-group-numbered w-100 d-flex p-0 scrollbar scrollbar-black bordered-black square thin border border-light rounded-lg overflow-auto flex-column mb-auto";
+        // myOl.style.width ="inherit"
+        // myOl.style.height ="inherit"
         for (i = 0; i < data.length; i++) {
           if (data[i].list === myHeader.innerHTML) {
             // const myNewItem = document.createElement("p");
@@ -280,25 +301,25 @@ function appendData(data) {
             // document.getElementById("newnew").appendChild(myNewItem);
             const myItImg = document.createElement("img");
             //myNewItem.className = "list-group-item list-group-item-action mb-0 opacity-75";
-            myItImg.src = "images/items/"+[i]+".jpg";
+            myItImg.src = "images/items/" + [i] + ".jpg";
             myItImg.className = "img-fluid";
-            myItImg.style = "width:40px; float:left;padding-right:5px;"
+            myItImg.style = "width:40px; float:left;padding-right:5px;";
             const myNewItem = document.createElement("li");
-            myNewItem.className = "list-group-item list-group-item-action mb-0 opacity-75";            
+            myNewItem.className =
+              "list-group-item list-group-item-action mb-0 opacity-75";
             myNewItem.innerHTML = data[i].item;
-            
-           //change bachground colour when item is clicked
-            myNewItem.onclick = function colorChange() {         
-              myNewItem.className="list-group-item active border border-secondary" 
-          }
-          myNewItem.appendChild(myItImg); 
-            myOl.appendChild(myNewItem);     
+
+            //change bachground colour when item is clicked
+            myNewItem.onclick = function colorChange() {
+              myNewItem.className =
+                "list-group-item active border border-secondary";
+            };
+            myNewItem.appendChild(myItImg);
+            myOl.appendChild(myNewItem);
           } else {
           }
         }
         document.getElementById("newnew").appendChild(myOl);
-     
-        
       }
       function comment21() {
         fetch("w_comments.json")
@@ -326,6 +347,12 @@ function appendData(data) {
               const myNewComDiv = document.createElement("div");
               const myNewNestComDiv = document.createElement("div");
               const myNewSmall = document.createElement("small");
+
+              let myAvi1 = document.createElement("img");
+              myAvi1.className = "img-fluid";
+              myAvi1.style =
+                "width:50px; height:50px; float:left; margin-right:10px; border: 0px solid red; border-radius: 50%;";
+              myAvi1.src = "images/avatars/1" + [i] + ".jpg";
               myNewA.className =
                 "list-group-item list-group-item-action d-flex gap-3 py-3";
               myNewComDiv.className =
@@ -337,7 +364,7 @@ function appendData(data) {
               myNewSmall.innerHTML = datac[i].time;
 
               myNewNestComDiv.appendChild(myNewItem);
-
+              myNewComDiv.appendChild(myAvi1);
               myNewComDiv.appendChild(myNewNestComDiv);
               myNewComDiv.appendChild(myNewSmall);
               myNewA.appendChild(myNewComDiv);
@@ -376,21 +403,21 @@ function appendData(data) {
               const myNewA1 = document.createElement("a");
               const myNewComDiv1 = document.createElement("div");
               const myNewNestComDiv1 = document.createElement("div");
-             // const myNewSmall1 = document.createElement("small");
+              // const myNewSmall1 = document.createElement("small");
               myNewA1.className =
                 "list-group-item list-group-item-action d-flex gap-3 py-3";
               myNewComDiv1.className =
                 "d-flex gap-2 w-100 justify-content-between";
               //myNewSmall1.className = "opacity-50 text-nowrap";
               myNewItem1.className = "mb-0 opacity-75";
-              
-              myNewItem1.innerHTML ="<b>Step "+ rnum++ +"</b>"+"<br>"+datac[i].recipe;
-              
 
-             // myNewNestComDiv1.appendChild(myNewItem1);
+              myNewItem1.innerHTML =
+                "<b>Step " + rnum++ + "</b>" + "<br>" + datac[i].recipe;
+
+              // myNewNestComDiv1.appendChild(myNewItem1);
 
               myNewComDiv1.appendChild(myNewItem1);
-             // myNewComDiv1.appendChild(myNewSmall1);
+              // myNewComDiv1.appendChild(myNewSmall1);
               myNewA1.appendChild(myNewComDiv1);
 
               document.getElementById("newr").appendChild(myNewA1);
@@ -399,34 +426,46 @@ function appendData(data) {
           }
         }
       }
-      if (recipeCheck=="Recipes | SHOPPER") {
+      if (recipeCheck == "Recipes | SHOPPER") {
         recipe();
       }
-      
     };
   }
 }
 
-
-
 function orderf() {
-  let person = prompt("You have selected " + order+ "."+" Please enter a Whatsapp contact to send the list", "254 704 186334");
+  let person = prompt(
+    "You have selected " +
+      order +
+      "." +
+      " Please enter a Whatsapp contact to send the list",
+    "254 704 186334"
+  );
 
   if (person != null) {
-    let wNumber =  "https://wa.me/"+person+"?text=Hello%20I'm%20interested%20in%20"+order+"%20from%20https://shopper.ke/"
-    
+    let wNumber =
+      "https://wa.me/" +
+      person +
+      "?text=Hello%20I'm%20interested%20in%20" +
+      order +
+      "%20from%20https://shopper.ke/";
+
     //remove spaces from string (url)
-    wNumber = wNumber.replace(/\s/g, '');
-   // console.log("https://wa.me/"+person+"?text=I'm%20interested%20in%20your%20car%20for%20sale"+ order);
+    wNumber = wNumber.replace(/\s/g, "");
+    // console.log("https://wa.me/"+person+"?text=I'm%20interested%20in%20your%20car%20for%20sale"+ order);
     window.open(wNumber);
     //console.log(wNumber);
   }
 }
 function deliverf() {
-  if (
-    confirm("Do you want to make a delivery of " + order + "?")
-  ) {
-    window.open("https://wa.me/254704186334?text=I'm%20interested%20in%20making%20a%20delivery.");
+  if (confirm("Do you want to make a delivery of " + order + "?")) {
+    window.open(
+      "https://wa.me/254704186334?text=I'm%20interested%20in%20making%20a%20delivery."
+    );
   }
 }
 
+console.log("All Users;");
+console.log(allUsers);
+console.log("Currently selected user;");
+console.log(selectedUser);
