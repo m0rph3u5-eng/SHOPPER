63f50b95d4a695def50e2937c18ef1176d639930
Mike Ambani <63499020+m0rph3u5-eng@users.noreply.github.com>
2022-11-29 00:16:12 -0800

    submit comment form complete with backend

--- pages/contact/contact.php
+++ pages/contact/contact.php
@@ -1,23 +1,23 @@
 <?php
   
 if($_POST) {
-    $visitor_name = "";
-    $visitor_email = "";
+    $name = "";
+    $email = "";
     $email_title = "";
     $concerned_department = "";
-    $visitor_message = "";
+    $subject = "";
     $email_body = "<div>";
       
     if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
         $email_body .= "<div>
                            <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
                         </div>";
     }
  
     if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
         $email_body .= "<div>
                            <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
                         </div>";
@@ -25,21 +25,21 @@ if($_POST) {
       
     if(isset($_POST['email_title'])) {
         $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
+        $subject .= "<div>
                            <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
                         </div>";
     }
       
     if(isset($_POST['concerned_department'])) {
         $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
+        $subject .= "<div>
                            <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
                         </div>";
     }
       
     if(isset($_POST['visitor_message'])) {
         $visitor_message = htmlspecialchars($_POST['visitor_message']);
-        $email_body .= "<div>
+        $subject .= "<div>
                            <label><b>Visitor Message:</b></label>
                            <div>".$visitor_message."</div>
                         </div>";
@@ -58,7 +58,7 @@ if($_POST) {
         $recipient = "contact@domain.com";
     }
       
-    $email_body .= "</div>";
+    $subject .= "</div>";
  
     $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
@@ -73,4 +73,18 @@ if($_POST) {
 } else {
     echo '<p>Something went wrong</p>';
 }
+
+if (! empty($_POST["send"])) {
+    $name = $_POST["userName"];
+    $email = $_POST["userEmail"];
+    $subject = $_POST["subject"];
+    $content = $_POST["content"];
+    $conn = mysqli_connect("localhost", "root", "test", "contactform_database") or die("Connection Error: " . mysqli_error($conn));
+    $stmt = $conn->prepare("INSERT INTO tblcontact (user_name, user_email, subject,content) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $subject, $content);
+    $stmt->execute();
+    $message = "Your contact information is saved successfully.";
+    $type = "success";
+    $stmt->close();
+    $conn->close();
 ?>
